400059b4e04ecb75407c574fb99f0a33b2661462
pelme <pelmenstar1@gmail.com>
2022-06-01 18:51:55 +0300

    Optimize AnimationHelper

--- library/src/main/java/io/github/pelmenstar1/rangecalendar/AnimationHelper.kt
+++ library/src/main/java/io/github/pelmenstar1/rangecalendar/AnimationHelper.kt
@@ -3,30 +3,20 @@ package io.github.pelmenstar1.rangecalendar
 import android.animation.ValueAnimator
 import android.os.Build
 import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.util.FloatProperty
 import android.annotation.SuppressLint
 
 internal object AnimationHelper {
-    private val BOXED_ZERO = java.lang.Float.valueOf(0f)
+    private val VALUES = PropertyValuesHolder.ofFloat("", 0f, 1f)
 
     @SuppressLint("Recycle")
     inline fun createFractionAnimator(crossinline callback: (Float) -> Unit): ValueAnimator {
-        return if (Build.VERSION.SDK_INT >= 24) {
-            ObjectAnimator().apply {
-                setProperty(object : FloatProperty<Any?>("property") {
-                    override fun get(o: Any?): Float? = BOXED_ZERO
-                    override fun setValue(o: Any?, value: Float) {
-                        callback(value)
-                    }
-                })
-                setFloatValues(0f, 1f)
-            }
-        } else {
-            ValueAnimator().apply {
-                setFloatValues(0f, 1f)
-                addUpdateListener { animation: ValueAnimator ->
-                    callback(animation.animatedValue as Float)
-                }
+        return ValueAnimator().apply {
+            setValues(VALUES)
+
+            addUpdateListener {
+                callback(it.animatedFraction)
             }
         }
     }
