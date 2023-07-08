0a147484c06376d9096b7038aa84697f08dd4ff6
Oleh <pelmenstar1@gmail.com>
2023-07-08 22:48:41 +0300

    Add more checks in Distance constructors

--- library/src/main/java/com/github/pelmenstar1/rangecalendar/Distance.kt
+++ library/src/main/java/com/github/pelmenstar1/rangecalendar/Distance.kt
@@ -2,12 +2,18 @@ package com.github.pelmenstar1.rangecalendar
 
 /**
  * Represents abstract concept of distance that can be either absolute or relative to another dimension.
+ *
+ * By the definition, distance can't be negative.
  */
 sealed class Distance {
     /**
      * Represents an absolute distance, i.e. it doesn't depend on any other dimension.
      */
-    data class Absolute(val value: Float) : Distance()
+    data class Absolute(val value: Float) : Distance() {
+        init {
+            require(value >= 0) { "value should be non-negative" }
+        }
+    }
 
     /**
      * Represents a relative dimension that depends on the dimension specified by [anchor].
@@ -15,8 +21,11 @@ sealed class Distance {
      * The relativity is expressed as a coefficient [fraction] that is multiplied to the absolute value, [anchor] represents.
      * The [fraction] is always in range `0..1`.
      */
-    // TODO: Add check whether fraction is [0; 1]
-    data class Relative(val fraction: Float, val anchor: RelativeAnchor) : Distance()
+    data class Relative(val fraction: Float, val anchor: RelativeAnchor) : Distance() {
+        init {
+            require (fraction in 0f..1f) { "fraction should be in the range 0..1" }
+        }
+    }
 
     /**
      * Represents various types of dimensions.
